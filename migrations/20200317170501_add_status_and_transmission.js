c78bc659626918035153454473ab894d568e0a18
liampmoore <16942895+liampmoore@users.noreply.github.com>
2020-03-17 17:08:49 -0400

    Second migration, added status and transmission type columns

--- migrations/20200317170501_add_status_and_transmission.js
+++ migrations/20200317170501_add_status_and_transmission.js
@@ -1,8 +1,13 @@
-
 exports.up = function(knex) {
-  return 
+  return knex.schema.table("cars", tbl => {
+    tbl.string("status", 80)
+    tbl.string("transmission", 80)
+  });
 };
 
 exports.down = function(knex) {
-  
+  return knex.schema.table("cars", tbl => {
+    tbl.dropColumn("status");
+    tbl.dropColumn("transmission")
+  });
 };
