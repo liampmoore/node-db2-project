2ae260b6fe2db3bc22175736c8aacef3eb847317
liampmoore <16942895+liampmoore@users.noreply.github.com>
2020-03-17 17:03:29 -0400

    Created seed file

--- seeds/cars.js
+++ seeds/cars.js
@@ -1,13 +1,13 @@
 
 exports.seed = function(knex) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('cars').del()
     .then(function () {
       // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+      return knex('cars').insert([
+        {id: 1, vin: '12345678901234567', make: "Toyota", model: "Speedcar", mileage: 60.42},
+        {id: 2, vin: '12345678901234568', make: "Heyundai", model: "Spinetta", mileage: 42.42},
+        {id: 3, vin: '12345678901234569', make: "Fjord", model: "Trucker", mileage: 59.49}
       ]);
     });
 };
