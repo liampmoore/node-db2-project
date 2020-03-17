a81b25d3acf25cb60f978bed3d6a0c0563d43ff7
liampmoore <16942895+liampmoore@users.noreply.github.com>
2020-03-17 16:56:36 -0400

    Built cars table schema

---
+++ migrations/20200317164430_car_table.js
@@ -0,0 +1,25 @@
+
+exports.up = function(knex) {
+  return knex.schema.createTable('cars', async tbl => {
+    tbl.increments();
+    tbl
+      .string("vin", 17)
+      .notNullable()
+      .unique()
+      .index();
+
+    tbl
+      .string("make", 255)
+      .notNullable();
+    tbl
+      .string("model", 255)
+      .notNullable();
+    tbl
+      .decimal("mileage", 5, 2)
+      .notNullable();
+  })
+};
+
+exports.down = function(knex) {
+  
+};
